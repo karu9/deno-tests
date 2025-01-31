0d5efea26cade41c2965763289242453ebe19fe1
picheryc <16350216+karu9@users.noreply.github.com>
2025-01-31 14:46:17 +0100

    feat: first commit

---
+++ src/main/kotlin/RunDenoTestAction.kt
@@ -0,0 +1,47 @@
+package com.example.deno
+
+import com.intellij.execution.RunManager
+import com.intellij.execution.RunnerAndConfigurationSettings
+import com.intellij.execution.configurations.GeneralCommandLine
+import com.intellij.execution.executors.DefaultRunExecutor
+import com.intellij.execution.runners.ExecutionEnvironmentBuilder
+import com.intellij.execution.runners.ProgramRunner
+import com.intellij.openapi.actionSystem.*
+import com.intellij.openapi.project.Project
+
+class RunDenoTestAction : AnAction("Run Deno Test") {
+    override fun actionPerformed(e: AnActionEvent) {
+        val project: Project = e.project ?: return
+        val editor = e.getData(CommonDataKeys.EDITOR) ?: return
+        val document = editor.document
+
+        val caretModel = editor.caretModel
+        val lineNumber = caretModel.logicalPosition.line
+        val lineText = document.getText(document.getLineStartOffset(lineNumber)..document.getLineEndOffset(lineNumber))
+
+        // Extract the test name from `Deno.test("test name")`
+        val testNameRegex = "Deno\\.test\\([\"'](.+?)[\"']".toRegex()
+        val match = testNameRegex.find(lineText)
+        val testName = match?.groupValues?.get(1) ?: return
+
+        runDenoTest(project, testName)
+    }
+
+    private fun runDenoTest(project: Project, testName: String) {
+        val command = GeneralCommandLine("deno", "test", "--filter", testName)
+        val runManager = RunManager.getInstance(project)
+        val settings: RunnerAndConfigurationSettings? = runManager.createConfiguration(
+            "Run Deno Test",
+            DenoTestRunConfigurationFactory(DenoTestRunConfigurationType())
+        )
+
+        settings?.let {
+            runManager.addConfiguration(it)
+            runManager.selectedConfiguration = it
+
+            val executor = DefaultRunExecutor.getRunExecutorInstance()
+            val environment = ExecutionEnvironmentBuilder.create(executor, it).build()
+            ProgramRunner.getRunner(DefaultRunExecutor.EXECUTOR_ID, environment)?.execute(environment)
+        }
+    }
+}
